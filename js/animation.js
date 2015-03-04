2bfbe064f6cf78882a272bda54b3d697ca350e9e
Yun.kou <koopking@gmail.com>
2015-03-05 00:25:29 +0800

    菜单动画

--- js/animation.js
+++ js/animation.js
@@ -7,7 +7,20 @@
  *
  */
 
-$(function(){
-    $('.m_menu_list').css('transform','translate(0, 0)');
-    $('.m_show_wp').css('transform','translate(0, 0)');
-});
+// 1.list页面列表飞入效果。
+(function ($) {
+    $(function () {
+        $('.m_menu_list').css('transform', 'translate(0, 0)');
+        $('.m_show_wp').css('transform', 'translate(0, 0)');
+    });
+
+}(window.Zepto));
+
+// 2.list下拉效果。
+(function ($) {
+    var $menuWarp = $('.s_menu_dl');
+    $menuWarp.on('click', 'dt', function () {
+        var $thisMenu = $(this).parent();
+        $thisMenu.toggleClass('active');
+    });
+}(window.Zepto));
--- js/publicmoblie.js
+++ js/publicmoblie.js
@@ -17,19 +17,7 @@ $(document).ready(function(){
 	$('.m_gps_close').click(function() {
 		$(this).parent('.m_gps_hide').hide();
 	});
-	//3.2左侧菜单
-	$(".s_menu_dl dt").click(function() {
-		$(".s_menu_dl dt").removeClass('list_gray');
-		$(".s_menu_dl dt").attr('attribute', 'value');
-		if($(this).attr("state")=="open"){
-			$(this).siblings("dd").hide();
-			$(this).attr("state","close");
-		}else{
-			$(this).siblings("dd").show();
-			$(this).attr("state","open");
-			$(this).addClass('list_gray');
-		}
-	});
+	//3.2左侧菜单--------[已经去除::Yun.kou]
 	//top按钮效果
  	window.onscroll = function(){
             var article_showHeight = $(window).height();
