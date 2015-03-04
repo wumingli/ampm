53e5f0a8911f45915080b7d797d0e370b1c5d165
武明礼 <mingli@sina.cn>
2015-03-04 10:20:07 +0800

    update

--- js/publicmoblie.js
+++ js/publicmoblie.js
@@ -1,6 +1,44 @@
 //放公共js使用
 
 $(document).ready(function(){
-	//3.2.1 index user
-
-})
+	//3.2位置定位 弹出
+	$('.gps_store').click(function() {
+		$(this).siblings('.m_gps_hide').show();
+	});
+	//3.2位置定位 关闭
+	$('.m_gps_close').click(function() {
+		$(this).parent('.m_gps_hide').hide();
+	});
+	//3.2左侧菜单
+	$(".s_menu_dl dt").click(function() {
+		$(".s_menu_dl dt").removeClass('list_gray');
+		$(".s_menu_dl dt").attr('attribute', 'value');
+		if($(this).attr("state")=="open"){
+			$(this).siblings("dd").hide();
+			$(this).attr("state","close");
+		}else{
+			$(this).siblings("dd").show();
+			$(this).attr("state","open");
+			$(this).addClass('list_gray');
+		}
+	});
+	//top按钮效果
+ 	window.onscroll = function(){
+            var article_showHeight = $(window).height();
+            var t = document.documentElement.scrollTop || document.body.scrollTop;   
+            if( t >= article_showHeight) {
+                $(".u-btn-top").show();
+            }
+            if ( t < article_showHeight) {
+                $(".u-btn-top").hide();
+            }
+         };
+	$('.u-btn-top').click(function(){
+	        window.scrollTo(0,0);
+	    });
+	//3.10支付订单关闭
+	$('#close_btn').click(function() {
+		$(this).parents('.m-modal').hide();
+		$('.m-opacity-layer').hide();
+	});
+});
